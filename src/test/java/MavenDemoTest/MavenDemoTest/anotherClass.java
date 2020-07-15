f7099f3f6d77eea1f6c9f9f49b47f27526b75cad
Shivainfy <shivabadiger7@gmail.com>
2020-07-15 05:09:10 -0230

    added two methods

---
+++ src/test/java/MavenDemoTest/MavenDemoTest/anotherClass.java
@@ -0,0 +1,17 @@
+package MavenDemoTest.MavenDemoTest;
+
+import org.testng.annotations.Test;
+
+public class anotherClass {
+	
+	@Test
+	public void print()
+	{
+		System.out.println("testing");
+	}
+	
+	public void login()
+	{
+		System.out.println("login");
+	}
+}
